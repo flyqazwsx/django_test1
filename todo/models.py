c20e050d116c7998b4f54722b7a4f74d94cce33c
fly <fly@gmail.com>
2022-10-17 21:43:10 +0800

    完成todo 資料表建立

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(blank=True, null=True)
+    imporant = models.BooleanField(default=False)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
