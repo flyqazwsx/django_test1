97bf73f5a7cc663233f0b23ff4b7f7dd7800f45f
fly <fly@gmail.com>
2022-10-26 18:57:15 +0800

    設定heroku上傳環境

--- todolist/settings.py
+++ todolist/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2-7v9%#8xz%886uu=3+m+vc-!=9&c1@&0z#+8zvl6ursci$k8$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
